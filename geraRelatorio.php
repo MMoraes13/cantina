ad879da99ac8cbe159b55d99be1087a4f9f31e04
MMoraes13 <mateus.moraes.gf@gmail.com>
2017-03-02 22:20:14 -0300

    alteração de query e criação de script para atualização dos nomes aguardando para ser usado após solução do problema da matricula 000272

--- geraRelatorio.php
+++ geraRelatorio.php
@@ -11,8 +11,8 @@
 
         $queryContaLancheMensal = "SELECT COUNT(*) as mensal FROM lanche_dia WHERE MONTH(data) = '$mes1' AND YEAR(data) = '$ano1';";
         $queryContaLancheTotal = "SELECT COUNT(*) as geral FROM lanche_dia;";
-        $queryAlunosQueLancharam = "SELECT aluno.nome, date_format(lanche_dia.data, '%d/%m/%Y') as dia, TIME(lanche_dia.data) as hora from aluno, lanche_dia WHERE aluno.id = lanche_dia.idAluno AND MONTH(data) = '$mes1' AND YEAR(data) = '$ano1' GROUP BY lanche_dia.data ORDER BY lanche_dia.data, aluno.nome;";
-        echo $queryContaLancheMensal;   
+        $queryAlunosQueLancharam = "SELECT aluno.nome, COUNT(lanche_dia.id) as contagem from aluno, lanche_dia WHERE aluno.id = lanche_dia.idAluno AND MONTH(data) = '$mes1' AND YEAR(data) = '$ano1' GROUP BY lanche_dia.data ORDER BY lanche_dia.data, aluno.nome;";
+        
 
         $valorContaLancheMensal = mysqli_query($mysqli, $queryContaLancheMensal);
         $valorContaLancheTotal = mysqli_query($mysqli, $queryContaLancheTotal);
@@ -28,9 +28,9 @@
             while($res = mysqli_fetch_array($alunosQueLancharam))
             {
               $nome = $res['nome'];
-              $data = $res['dia'];
-              $hora = $res['hora']; 
-              $alunos .= "<pre><tr> <td> ".$nome." </td><td> ".$data." </td><td> ".$hora." </td></tr></pre>";
+              $contagem = $res['contagem'];
+              
+              $alunos .= "<pre><tr> <td> ".$nome." </td><td> ".$contagem." </td></tr></pre>";
             }
             $alunos .= "</table>";
             $_SESSION['listaAlunos'] = $alunos;
@@ -39,7 +39,8 @@
         header("Location: visualizaRelatorio.php");
         
         } catch (mysqli_sql_exception $e) { 
-          header("Location: erro_banco.html");
+          echo $queryAlunosQueLancharam;
+          //header("Location: erro_banco.html");
         }
       } else {
         header("Location: acessonegado.php");
